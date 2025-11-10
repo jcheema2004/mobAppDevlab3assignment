db6224489f86dc696f4d73ac6cacf92842723335
Jasmine Cheema <cheemajasmine2004@gmail.com>
2025-11-10 12:05:42 -0700

    set it up and update

---
+++ App.jsx
@@ -0,0 +1,34 @@
+/**
+ * My To Do List App
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet
+} from 'react-native';
+import ToDoList from './ToDoList';
+import ToDoForm from './ToDoForm';
+import { useState } from 'react';
+
+function App() {
+
+  const []
+
+  return (
+
+    <SafeAreaView >
+      <ToDoList />
+      <ToDoForm />
+    </SafeAreaView>
+
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+});
+
+export default App;
